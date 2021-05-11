dbba8230eb6bdbf565e79627dda05330fefbe78f
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-11 08:15:08 +0000

    search function

---
+++ get_op_func.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * get_op_func- searches instruction for opcode functions
+ * @str: opcode to search for
+ * Return: ptr to the desired function otherwise opcode_error
+ */
+void (*get_op_func(char *str))(stack_t **, unsigned int)
+{
+	instruction_t instructions[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
+		{"add", op_add},
+		{"nop", op_nop},
+		{"#", op_nop},
+		{NULL, NULL}
+	};
+	int idx = 0;
+
+	while (instructions[idx].opcode)
+	{
+		if (strcmp(instructions[idx].opcode, str) == 0)
+			return (instructions[idx].f);
+		idx++;
+	}
+/* opcode error */
+	return (opcode_error);
+}
