70612b0fb7c4b375549f1c943871cace83b443c0
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-13 12:10:44 +0000

    op

--- op_push.c
+++ op_push.c
@@ -1,10 +1,8 @@
 #include "monty.h"
 /**
  * op_push - pushes a value to the top of the stack
- * @head: double pointer to head node
+ * @head: A pointer to head node
  * @line_number: line number where opcode was encountered
- *
- * Return: nothing, exit with code EXIT_FAILURE on failure
  */
 void op_push(stack_t **head, unsigned int line_number)
 {
@@ -18,7 +16,6 @@ void op_push(stack_t **head, unsigned int line_number)
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 		malloc_error(head);
-
 /* make sure string contains only numbers and '-' */
 	while (stack_val.n[idx] != '\0')
 	{
