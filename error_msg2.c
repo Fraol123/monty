187ce61134ba8987c60e138fc88d686b0e37f50f
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-17 15:15:45 +0000

    berry error

--- error_msg2.c
+++ error_msg2.c
@@ -1,17 +1,6 @@
 #include "monty.h"
 
-/**
- * sub_error - handles error for op_sub function
- * @stack: a pointer to head node
- * @line_number: line number where error occured
- *
- */
-void sub_error(stack_t **stack, unsigned int line_number)
-{
-	fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-	free_stack(stack);
-	exit(EXIT_FAILURE);
-}
+
 /**
  * div_error - handles error for op_div function
  * @stack: a  pointer to head node
--- error_msg3.c
+++ error_msg3.c
@@ -12,3 +12,14 @@ void char_error(stack_t **stack, unsigned int line_number)
 	free_stack(stack);
 	exit(EXIT_FAILURE);
 }
+/**
+ *sub_error- handles error for op_sub
+ *@stack: a ptr to head node
+ *@line_number: a line wear error occured
+ */
+void sub_error(stack_t **stack, unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+	free_stack(stack);
+	exit(EXIT_FAILURE);
+}
